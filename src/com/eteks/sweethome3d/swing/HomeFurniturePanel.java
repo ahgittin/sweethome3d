3e4f5ee18aa00ca0c227a9afab2293f337139c70
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-08-26 00:11:09 +0100

    allow negative elevation for furniture, as per http://sourceforge.net/p/sweethome3d/feature-requests/488/

--- src/com/eteks/sweethome3d/swing/HomeFurniturePanel.java
+++ src/com/eteks/sweethome3d/swing/HomeFurniturePanel.java
@@ -295,7 +295,7 @@ public class HomeFurniturePanel extends JPanel implements DialogView {
       this.elevationLabel = new JLabel(SwingTools.getLocalizedLabelText(preferences, HomeFurniturePanel.class,
           "elevationLabel.text", unitName));
       final NullableSpinner.NullableSpinnerLengthModel elevationSpinnerModel = new NullableSpinner.NullableSpinnerLengthModel(
-          preferences, 0f, preferences.getLengthUnit().getMaximumElevation());
+          preferences, -preferences.getLengthUnit().getMaximumElevation(), preferences.getLengthUnit().getMaximumElevation());
       this.elevationSpinner = new NullableSpinner(elevationSpinnerModel);
       elevationSpinnerModel.setNullable(controller.getElevation() == null);
       elevationSpinnerModel.setLength(controller.getElevation());
